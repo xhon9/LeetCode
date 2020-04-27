03c366506e31d2d5f6866f954d901f6e5280fbb6
Ivan <59091671+xhon9@users.noreply.github.com>
2020-04-27 10:32:13 +0200

    Add files via upload

---
+++ Events/30_Day_CodingChallenge/25_JumpGame.py
@@ -0,0 +1,6 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        last_index = len(nums)-1
+        for i in range(last_index+1)[::-1]:
+            if i+nums[i] >= last_index: last_index = i
+        return not last_index
