111a12d00834e3631cc450150c03612155eba2c8
Ivan <ivangallo99@gmail.com>
2020-04-30 13:42:42 +0200

    Add files via upload

---
+++ Solved/+100/124_BinaryTreeMaximumPathSum.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root):
+        self.max = float('-inf')
+
+        def get_sum(root):
+            if not root: return 0
+            ls = max(get_sum(root.left), 0)
+            rs = max(get_sum(root.right), 0)
+            self.max = max(self.max, ls + rs + root.val)
+            return max(ls, rs, 0) + root.val
+
+        get_sum(root)
+        return self.max
